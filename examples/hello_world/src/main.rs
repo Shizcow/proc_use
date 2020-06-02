08d48a04f3d5d5b3f7805588f79436b59faaaec7
Shizcow <pohl.devin@gmail.com>
2020-06-01 20:54:05 -0600

    The very most basic functionality

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,3 +1,9 @@
+mod foo; // TODO proc_mod
+
+proc_use::proc_use! {
+    ["foo"]
+}
+
 fn main() {
-    println!("Hello, world!");
+    foo();
 }
--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use proc_macro::TokenStream;
+use syn::{Expr, Lit};
+use quote::quote;
+use proc_macro2::{Ident, Span};
+
+#[proc_macro]
+pub fn proc_use(input: TokenStream) -> TokenStream {
+    if let Ok(items) = syn::parse::<Expr>(input) {
+	if let Expr::Array(arr) = items {
+	    for elem in arr.elems.into_iter() { // TODO concat the quotes
+		if let Expr::Lit(lit) = elem {
+		    if let Lit::Str(string) = lit.lit {
+			let use_ident = Ident::new(&string.value(),
+						   Span::call_site());
+			return TokenStream::from(quote! {
+			    use #use_ident::*;
+			})
+		    } // TODO: compiler error "must be a string"
+		} // TODO: compiler error "need lit"
+	    }
+	} // TODO: env!()
+    } // TODO: compiler error on else
+    
+    TokenStream::new()
 }
