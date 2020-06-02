dda4297f3d6c447b03e6ad82302a57ca13a0b6c0
Jonathan Pavlik <jonathan.t.pavlik@gmail.com>
2020-06-02 14:50:40 -0400

    we can now tell if pkg has a valid attr if it exists

--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -16,9 +16,9 @@ fn mk_err<T: quote::ToTokens>(t: T, msg: String) -> syn::Error {
     syn::Error::new_spanned(t, msg)
 }
 
-fn ident_match(term: &str, ident: syn::Ident) -> syn::Result<()> {
+fn ident_match(term: &str, ident: syn::Ident) -> syn::Result<bool> {
     if ident.to_string().as_str() == term {
-	return Ok(());
+	return Ok(true);
     }
     
     Err(syn::Error::new(
@@ -27,7 +27,7 @@ fn ident_match(term: &str, ident: syn::Ident) -> syn::Result<()> {
     ))
 }
 
-fn has_attr(attr: &str, item: syn::ItemUse) -> syn::Result<()> {
+fn has_attr(attr: &str, item: syn::ItemUse) -> syn::Result<bool> {
     let num_attrs = item.attrs.len();
     if num_attrs == 1 {
 	let segments = item.attrs[0].path.segments.clone();
@@ -41,24 +41,31 @@ fn has_attr(attr: &str, item: syn::ItemUse) -> syn::Result<()> {
 
 	ident_match(attr, segments[0].ident.clone())?;
     } else if num_attrs > 1 {
-	// return Ok(());
 	return Err(mk_err(
 	    item,
 	    format!("Error: expected 1 attribute but recieved {}.", num_attrs)
 	));
     }
     
-    Ok(())
+    Ok(false)
 }
 
 impl ProcUseMacroInput {
     fn expand(&self) -> proc_macro2::TokenStream  {
+	
 	for item_outer in self.items.clone() {
 	    match item_outer {
 		syn::Item::Use(item_use) => {
 		    
 		    println!("{:#?}", item_use);
-		    has_attr("", item_use);
+		    let res = has_attr("mod_field", item_use);
+
+		    match res {
+			Ok(has_attr) => {
+			    println!("I have attr and its valid");
+			},
+			Err(err) => return err.to_compile_error()
+		    }
 		},
 		_ => {
 		    println!("fail");
