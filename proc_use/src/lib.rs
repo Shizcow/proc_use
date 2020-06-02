0b56519588f7d441daee4dc42eec3aa5bacbfc1b
Shizcow <pohl.devin@gmail.com>
2020-06-01 22:00:30 -0600

    Added a few more compiler errors

--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -1,40 +1,49 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use syn::{Expr, Lit, ItemUse, spanned::Spanned};
 use quote::quote;
 
 #[proc_macro]
 pub fn proc_use(input: TokenStream) -> TokenStream {
     if let Ok(items) = syn::parse::<Expr>(input) {
-	if let Expr::Array(arr) = items {
-	    let mut uses = Vec::new();
-	    for elem in arr.elems.into_iter() {
-		let error = match elem {
-		    Expr::Lit(lit) =>
-			match lit.lit {
-			    Lit::Str(string) =>
-				match syn::parse_str::<ItemUse>
-				(&format!("use {};", string.value())) {
-				    Ok(item) => {
-					uses.push(item);
-					continue;
+	match items {
+	    Expr::Array(arr) => {
+		let mut uses = Vec::new();
+		for elem in arr.elems.into_iter() {
+		    let error = match elem {
+			Expr::Lit(lit) =>
+			    match lit.lit {
+				Lit::Str(string) =>
+				    match syn::parse_str::<ItemUse>
+				    (&format!("use {};", string.value())) {
+					Ok(item) => {
+					    uses.push(item);
+					    continue;
+					},
+					Err(error) => error.span(),
 				    },
-				    Err(error) => error.span(),
-				},
-			    error => error.span(),
-			},
-		    error => error.span(),
-		};
-		return syn::Error::new(
-		    error,
-		    "Invalid format for use. You just want the import path, eg: \
-		     \"foo::{bar, baz}\""
-		).to_compile_error().into();
-	    }
-	    return TokenStream::from(quote! {
-		#(#uses)*
-	    })
-	} // TODO: env!()
-    } // TODO: compiler error on else
-    
-    TokenStream::new()
+				error => error.span(),
+			    },
+			error => error.span(),
+		    };
+		    return syn::Error::new(
+			error,
+			"Invalid format for use. You just want the import path, eg: \
+			 \"foo::{bar, baz}\""
+		    ).to_compile_error().into();
+		}
+		return TokenStream::from(quote! {
+		    #(#uses)*
+		})
+	    },
+	    other => return syn::Error::new(
+		other.span(),
+		"I can't yet parse this item"
+	    ).to_compile_error().into(),
+	}
+    }
+    syn::Error::new(
+	Span::call_site(),
+	"Syntax Error"
+    ).to_compile_error().into()
 }
