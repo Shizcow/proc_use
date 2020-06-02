ad18d5ddcf6158385325a219aa10c4caedea0c85
Shizcow <pohl.devin@gmail.com>
2020-06-01 21:10:43 -0600

    Arbitrarily complex use statements

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -2,7 +2,7 @@ mod foo; // TODO proc_mod
 mod bar;
 
 proc_use::proc_use! {
-    ["foo", "bar"]
+    ["foo::*", "bar::bar"]
 }
 
 fn main() {
--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -1,23 +1,22 @@
 use proc_macro::TokenStream;
-use syn::{Expr, Lit};
+use syn::{Expr, Lit, ItemUse};
 use quote::quote;
-use proc_macro2::{Ident, Span};
 
 #[proc_macro]
 pub fn proc_use(input: TokenStream) -> TokenStream {
     if let Ok(items) = syn::parse::<Expr>(input) {
 	if let Expr::Array(arr) = items {
-	    let idents = arr.elems.into_iter().map(|elem| {
+	    let uses = arr.elems.into_iter().map(|elem| {
 		if let Expr::Lit(lit) = elem {
 		    if let Lit::Str(string) = lit.lit {
-			return Ident::new(&string.value(),
-						   Span::call_site());
+			return syn::parse_str::<ItemUse>(&format!("use {};", string.value()))
+			    .unwrap();
 		    } // TODO: compiler error "must be a string"
 		} // TODO: compiler error "need lit"
 		panic!("I don't have error reporting yet");
 	    });
 	    return TokenStream::from(quote! {
-		#(use #idents::*;)*
+		#(#uses)*
 	    })
 	} // TODO: env!()
     } // TODO: compiler error on else
