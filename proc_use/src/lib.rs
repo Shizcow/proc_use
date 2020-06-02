23e71a93d00982dd67eced2281578fe205451cc2
Shizcow <pohl.devin@gmail.com>
2020-06-01 21:34:44 -0600

    Cleaned up some error reporting

--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -1,5 +1,5 @@
 use proc_macro::TokenStream;
-use syn::{Expr, Lit, ItemUse};
+use syn::{Expr, Lit, ItemUse, spanned::Spanned};
 use quote::quote;
 
 #[proc_macro]
@@ -8,21 +8,27 @@ pub fn proc_use(input: TokenStream) -> TokenStream {
 	if let Expr::Array(arr) = items {
 	    let mut uses = Vec::new();
 	    for elem in arr.elems.into_iter() {
-		if let Expr::Lit(lit) = elem {
-		    if let Lit::Str(string) = lit.lit {
-			match syn::parse_str::<ItemUse>(&format!("use {};", string.value())) {
-			    Ok(item) => {
-				uses.push(item);
-				continue;
-			    },
-			    Err(_) => return syn::Error::new(
-				string.span(),
-				"Invalid format for use"
-			    ).to_compile_error().into(),
-			}
-		    } // TODO: compiler error "must be a string"
-		} // TODO: compiler error "need lit"
-		panic!("I don't have intelligent error reporting yet");
+		let error = match elem {
+		    Expr::Lit(lit) =>
+			match lit.lit {
+			    Lit::Str(string) =>
+				match syn::parse_str::<ItemUse>
+				(&format!("use {};", string.value())) {
+				    Ok(item) => {
+					uses.push(item);
+					continue;
+				    },
+				    Err(error) => error.span(),
+				},
+			    error => error.span(),
+			},
+		    error => error.span(),
+		};
+		return syn::Error::new(
+		    error,
+		    "Invalid format for use. You just want the import path, eg: \
+		     \"foo::{bar, baz}\""
+		).to_compile_error().into();
 	    }
 	    return TokenStream::from(quote! {
 		#(#uses)*
