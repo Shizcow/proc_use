f6499208ac36e4c74d41b93ad7aea749e14ed7e1
Jonathan Pavlik <jonathan.t.pavlik@gmail.com>
2020-06-02 13:34:19 -0400

    started custom syntax parser

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,11 +1,11 @@
-mod foo; // TODO proc_mod
-mod bar;
-
-proc_use::proc_use! {
-    ["foo::*", "bar::bar"]
-}
+proc_use::proc_use_inline!(mod_all
+               {
+                   #[disable]
+                   use foo::*;
+                   use bar::bar;
+               }
+);
 
 fn main() {
-    foo();
-    bar();
+    println!("hi");
 }
--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -1,49 +1,109 @@
+extern crate proc_macro;
+
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use syn::{Expr, Lit, ItemUse, spanned::Spanned};
+use syn::{parse_macro_input, Result, Token};
+use syn::parse::{Parse, ParseStream};
 use quote::quote;
 
-#[proc_macro]
-pub fn proc_use(input: TokenStream) -> TokenStream {
-    if let Ok(items) = syn::parse::<Expr>(input) {
-	match items {
-	    Expr::Array(arr) => {
-		let mut uses = Vec::new();
-		for elem in arr.elems.into_iter() {
-		    let error = match elem {
-			Expr::Lit(lit) =>
-			    match lit.lit {
-				Lit::Str(string) =>
-				    match syn::parse_str::<ItemUse>
-				    (&format!("use {};", string.value())) {
-					Ok(item) => {
-					    uses.push(item);
-					    continue;
-					},
-					Err(error) => error.span(),
-				    },
-				error => error.span(),
-			    },
-			error => error.span(),
-		    };
-		    return syn::Error::new(
-			error,
-			"Invalid format for use. You just want the import path, eg: \
-			 \"foo::{bar, baz}\""
-		    ).to_compile_error().into();
-		}
-		return TokenStream::from(quote! {
-		    #(#uses)*
-		})
+#[derive(Debug)]
+struct ProcUseMacroInput {
+    ident: syn::Ident,
+    brace_token: syn::token::Brace,
+    items: Vec<syn::Item>,
+}
+
+fn ident_match(term: String, ident: syn::Ident) -> String {
+    if ident.to_string() == term {
+	return term;
+    }
+    "".to_string()
+}
+
+fn has_attr(attr: String, item: syn::ItemUse) -> bool {
+    if item.attrs.len() == 1 {
+	let segments = item.attrs[0].path.segments.clone();
+	if segments.len() < 1 || segments.len() > 1 {
+	    return false;
+	}
+	
+	println!("{:#?}", item.attrs[0].path.segments);
+	return true;
+    }
+
+    false
+}
+
+fn expand_mod_all(items: Vec<syn::Item>,) -> proc_macro2::TokenStream {
+    for item_outer in items {
+	match item_outer {
+	    syn::Item::Use(item_use) => {
+		
+		// println!("{:#?}", item_use);
+		has_attr("disabled".to_string(), item_use);
 	    },
-	    other => return syn::Error::new(
-		other.span(),
-		"I can't yet parse this item"
-	    ).to_compile_error().into(),
+	    _ => {
+		println!("fail");
+	    }
+	}
+	
+    }
+    proc_macro2::TokenStream::new()
+}
+
+fn expand_mod_none(items: Vec<syn::Item>,) -> proc_macro2::TokenStream {
+    proc_macro2::TokenStream::new()
+}
+
+impl ProcUseMacroInput {
+    fn expand(&self) -> proc_macro2::TokenStream  {
+	match self.ident.to_string().as_str() {
+	    "mod_all" => {
+		expand_mod_all(self.items.clone())
+	    },
+	    "mod_none" => {
+		expand_mod_none(self.items.clone())
+	    },
+	    _ => {
+		syn::Error::new(
+			self.ident.span(),
+			"Invalid identifier. Use either mod_all or mod_none."
+		    ).to_compile_error().into()	
+	    }
 	}
     }
-    syn::Error::new(
-	Span::call_site(),
-	"Syntax Error"
-    ).to_compile_error().into()
+}
+
+impl Parse for ProcUseMacroInput {
+    fn parse(input: ParseStream) -> Result<Self> {
+	let content;
+
+        Ok(ProcUseMacroInput {
+	    ident: syn::Ident::parse(input)?,
+	    brace_token: syn::braced!(content in input),
+	    items: {
+		let mut items = Vec::new();
+                while !content.is_empty() {
+                    items.push(content.parse()?);
+                }
+                items
+	    },
+        })
+    }
+}
+
+// impl Into<proc_macro2::TokenStream> for ProcUseMacroInput {
+//     fn into(self) -> proc_macro2::TokenStream {
+//         self.expand(self.tt.clone())
+//     }
+// }
+
+#[proc_macro]
+pub fn proc_use_inline(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as ProcUseMacroInput);
+    let output = input.expand();
+    // println!("{:#?}", input);
+    // println!("{:#?}", output);
+    
+    TokenStream::new()
 }
