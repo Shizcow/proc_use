4b5a1f8156a7b0b205b34e27da7c0ff174d20fe6
Shizcow <pohl.devin@gmail.com>
2020-06-03 20:18:31 -0600

    I know how to use git I promise

--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -1,57 +1,5 @@
 extern crate proc_macro;
 
-<<<<<<< HEAD
-use proc_macro::{TokenStream, TokenTree::{self, Punct, Group}, Delimiter::Bracket, Ident};
-use syn::{File, parse_macro_input};
-
-// replaces keyword `mod` with `__mod` in attribute contexts
-fn sanitize(input: TokenStream) -> TokenStream {
-    let mut tokens: Vec<TokenTree> = input.into_iter().collect();
-    for i in 0..tokens.len()-1 {
-	match &tokens[i] {
-	    Punct(p) if p.as_char() == '#' => {
-		match &tokens[i+1] {
-		    Group(g) if g.delimiter() == Bracket => {
-			let mut stream: Vec<TokenTree> = g.stream().into_iter().collect();
-			if stream.len() == 1 && stream[0].to_string() == "mod" {
-			    stream[0] = TokenTree::Ident(Ident::new("__mod", stream[0].span()));
-			}
-			tokens[i+1] = Group(proc_macro::Group::new(Bracket, stream.into_iter().collect()));
-		    },
-		    Punct(p) if p.as_char() == '!' => {
-			match &tokens[i+2] {
-			    Group(g) if g.delimiter() == Bracket => {
-				let mut stream: Vec<TokenTree> = g.stream().into_iter().collect();
-				if stream.len() == 1 && stream[0].to_string() == "mod" {
-				    stream[0] = TokenTree::Ident(Ident::new("__mod", stream[0].span()));
-				}
-				tokens[i+2] = Group(proc_macro::Group::new(Bracket, stream.into_iter().collect()));
-			    },
-			    _ => {},
-			}
-		    },
-		    _ => {},
-		}
-	    },
-	    _ => {},
-	}
-    }
-    
-    tokens.into_iter().collect()
-}
-
-#[proc_macro]
-pub fn proc_use(input: TokenStream) -> TokenStream {
-    let input = sanitize(input);
-    // because `mod` is a keyword, replace it in attributes with `__mod`
-    // This is done without regex in order to preserve span info
-    
-    let parsed = parse_macro_input!(input as File);
-
-    panic!("{:#?}", parsed);
-
-    //TokenStream::new()
-=======
 use syn::{File, parse_macro_input};
 use proc_macro::{Delimiter, TokenStream, TokenTree, Ident};
 use quote::quote;
@@ -189,5 +137,4 @@ fn sanitize(input: TokenStream) -> TokenStream {
 pub fn proc_use_inline(input: TokenStream) -> TokenStream {
     let input = sanitize(input);
     expand(parse_macro_input!(input as File).items)
->>>>>>> custom_syntax
 }
