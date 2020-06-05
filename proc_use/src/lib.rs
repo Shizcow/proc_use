e106eea622eca6e6f48b239f5ef7c010f87a2e8e
Shizcow <pohl.devin@gmail.com>
2020-06-05 17:20:44 -0600

    Cleaned up file functions

--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -11,7 +11,7 @@ impl UseBuilder {
     pub fn new() -> Self {
 	Self{mod_stmts: Vec::new(), use_stmts: Vec::new()}
     }
-    pub fn mod_file(&mut self, file: PathBuf) -> &mut Self {
+    fn file(&mut self, file: PathBuf, use_stmt: Option<String>) {
 	let file = match file.canonicalize() {
 	    Ok(file) => file,
 	    Err(err) => panic!("Could not canonicalize file '{}': {}", file.to_string_lossy(), err),
@@ -19,52 +19,18 @@ impl UseBuilder {
 	match (file.file_stem(), file.extension()) {
 	    (Some(mod_name), Some(ext)) if ext == "rs" => {
 		self.mod_stmts.push(format!("#[path = \"{}\"]\nmod {};",
-					file.to_string_lossy(), mod_name.to_string_lossy()));
+					    file.to_string_lossy(), mod_name.to_string_lossy()));
+		if let Some(use_stmt) = use_stmt {
+		    self.use_stmts.push(format!("use {}::{};", mod_name.to_string_lossy(), use_stmt));
+		}
 	    },
 	    (Some(_), _) => panic!("Invalid file '{}'. Probable cause: file is not a rust file.",
 				   file.to_string_lossy()),
 	    (None, _) => panic!("Invalid file '{}'. Probable cause: is not a regular file.",
 				file.to_string_lossy()),
 	}
-	self
     }
-    pub fn use_file(&mut self, file: PathBuf, use_stmt: String) -> &mut Self {
-	let file = match file.canonicalize() {
-	    Ok(file) => file,
-	    Err(err) => panic!("Could not canonicalize file '{}': {}", file.to_string_lossy(), err),
-	};
-	match (file.file_stem(), file.extension()) {
-	    (Some(mod_name), Some(ext)) if ext == "rs" => {
-		self.mod_stmts.push(format!("#[path = \"{}\"]\nmod {};",
-					file.to_string_lossy(), mod_name.to_string_lossy()));
-		self.use_stmts.push(format!("use {}::{};", mod_name.to_string_lossy(), use_stmt));
-	    },
-	    (Some(_), _) => panic!("Invalid file '{}'. Probable cause: file is not a rust file.",
-				   file.to_string_lossy()),
-	    (None, _) => panic!("Invalid file '{}'. Probable cause: is not a regular file.",
-				file.to_string_lossy()),
-	}
-	self
-    }
-    pub fn use_crate(&mut self, use_stmt: String) -> &mut Self {
-	self.use_stmts.push(format!("use {};", use_stmt));
-	self
-    }
-    pub fn write_to_file_use(&mut self, path: PathBuf) -> &mut Self {
-	self.__write_to_file(path, self.use_stmts.join("\n").as_bytes());
-	self
-    }
-    pub fn write_to_file_mod(&mut self, path: PathBuf) -> &mut Self {
-	self.__write_to_file(path, self.mod_stmts.join("\n").as_bytes());
-	self
-    }
-    pub fn write_to_file_all(&mut self, path: PathBuf) -> &mut Self {
-	self.__write_to_file(path, itertools::join(
-	    self.mod_stmts.iter().chain(self.use_stmts.iter()), "\n"
-	).as_bytes());
-	self
-    }
-    fn __write_to_file(&mut self, path: PathBuf, contents: &[u8]) {
+    fn write_to_file(&mut self, path: PathBuf, contents: &[u8]) {
 	match File::create(&path) {
 	    Ok(mut file) => {
 		if let Err(err) = file.write_all(contents) {
@@ -74,5 +40,32 @@ impl UseBuilder {
 	    Err(err) => panic!("Could not open file '{}' for writing: {}",
 			       path.to_string_lossy(), err),
 	}
+    }
+    pub fn mod_file(&mut self, file: PathBuf) -> &mut Self {
+	self.file(file, None);
+	self
+    }
+    pub fn use_file(&mut self, file: PathBuf, use_stmt: String) -> &mut Self {
+	self.file(file, Some(use_stmt));
+	self
+    }
+    pub fn use_crate(&mut self, use_stmt: String) -> &mut Self {
+	self.use_stmts.push(format!("use {};", use_stmt));
+	self
+    }
+    
+    pub fn write_to_file_use(&mut self, path: PathBuf) -> &mut Self {
+	self.write_to_file(path, self.use_stmts.join("\n").as_bytes());
+	self
+    }
+    pub fn write_to_file_mod(&mut self, path: PathBuf) -> &mut Self {
+	self.write_to_file(path, self.mod_stmts.join("\n").as_bytes());
+	self
+    }
+    pub fn write_to_file_all(&mut self, path: PathBuf) -> &mut Self {
+	self.write_to_file(path, itertools::join(
+	    self.mod_stmts.iter().chain(self.use_stmts.iter()), "\n"
+	).as_bytes());
+	self
     }
 }
