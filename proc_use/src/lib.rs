5a6ae155d9d32aafbc80f8f64430380da5f2ee13
Shizcow <pohl.devin@gmail.com>
2020-06-05 17:07:10 -0600

    Added richer write_to_file funcitons

--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -3,12 +3,13 @@ use std::fs::File;
 use std::io::Write;
 
 pub struct UseBuilder {
-    stmts: Vec<String>,
+    mod_stmts: Vec<String>,
+    use_stmts: Vec<String>,
 }
 
 impl UseBuilder {
     pub fn new() -> Self {
-	Self{stmts: Vec::new()}
+	Self{mod_stmts: Vec::new(), use_stmts: Vec::new()}
     }
     pub fn mod_file(&mut self, file: PathBuf) -> &mut Self {
 	let file = match file.canonicalize() {
@@ -17,7 +18,7 @@ impl UseBuilder {
 	};
 	match (file.file_stem(), file.extension()) {
 	    (Some(mod_name), Some(ext)) if ext == "rs" => {
-		self.stmts.push(format!("#[path = \"{}\"]\nmod {};",
+		self.mod_stmts.push(format!("#[path = \"{}\"]\nmod {};",
 					file.to_string_lossy(), mod_name.to_string_lossy()));
 	    },
 	    (Some(_), _) => panic!("Invalid file '{}'. Probable cause: file is not a rust file.",
@@ -34,8 +35,9 @@ impl UseBuilder {
 	};
 	match (file.file_stem(), file.extension()) {
 	    (Some(mod_name), Some(ext)) if ext == "rs" => {
-		self.stmts.push(format!("#[path = \"{}\"]\nmod {};\nuse {}::{};", file.to_string_lossy(),
-					mod_name.to_string_lossy(), mod_name.to_string_lossy(), use_stmt));
+		self.mod_stmts.push(format!("#[path = \"{}\"]\nmod {};",
+					file.to_string_lossy(), mod_name.to_string_lossy()));
+		self.use_stmts.push(format!("use {}::{};", mod_name.to_string_lossy(), use_stmt));
 	    },
 	    (Some(_), _) => panic!("Invalid file '{}'. Probable cause: file is not a rust file.",
 				   file.to_string_lossy()),
@@ -45,13 +47,27 @@ impl UseBuilder {
 	self
     }
     pub fn use_crate(&mut self, use_stmt: String) -> &mut Self {
-	self.stmts.push(format!("use {};", use_stmt));
+	self.use_stmts.push(format!("use {};", use_stmt));
 	self
     }
-    pub fn write_to_file(&mut self, path: PathBuf) {
+    pub fn write_to_file_use(&mut self, path: PathBuf) -> &mut Self {
+	self.__write_to_file(path, self.use_stmts.join("\n").as_bytes());
+	self
+    }
+    pub fn write_to_file_mod(&mut self, path: PathBuf) -> &mut Self {
+	self.__write_to_file(path, self.mod_stmts.join("\n").as_bytes());
+	self
+    }
+    pub fn write_to_file_all(&mut self, path: PathBuf) -> &mut Self {
+	self.__write_to_file(path, itertools::join(
+	    self.mod_stmts.iter().chain(self.use_stmts.iter()), "\n"
+	).as_bytes());
+	self
+    }
+    fn __write_to_file(&mut self, path: PathBuf, contents: &[u8]) {
 	match File::create(&path) {
 	    Ok(mut file) => {
-		if let Err(err) = file.write_all(self.stmts.join("\n").as_bytes()) {
+		if let Err(err) = file.write_all(contents) {
 		    panic!("Could not write to file '{}': {}", path.to_string_lossy(), err);
 		}
 	    },
