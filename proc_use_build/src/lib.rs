ddd728a28f27ec893dbbe6682199ca0a0601b3e8
Shizcow <pohl.devin@gmail.com>
2020-06-05 09:09:45 -0600

    More work on proc_macro_build

--- proc_use_build/src/lib.rs
+++ proc_use_build/src/lib.rs
@@ -1,40 +1,62 @@
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Write;
-use quote::quote;
 
 pub struct UseBuilder {
-    files: Vec<PathBuf>,
+    stmts: Vec<String>,
 }
 
 impl UseBuilder {
     pub fn new() -> Self {
-	Self{files: Vec::new()}
+	Self{stmts: Vec::new()}
     }
-    pub fn add_file(&mut self, file: PathBuf) -> &mut Self { // TODO: template String
-	self.files.push(file);
+    pub fn mod_file(&mut self, file: PathBuf) -> &mut Self {
+	let file = match file.canonicalize() {
+	    Ok(file) => file,
+	    Err(err) => panic!("Could not canonicalize file '{}': {}", file.to_string_lossy(), err),
+	};
+	match (file.file_stem(), file.extension()) {
+	    (Some(mod_name), Some(ext)) if ext == "rs" => {
+		self.stmts.push(format!("#[path = \"{}\"]\nmod {};\n",
+					file.to_string_lossy(), mod_name.to_string_lossy()));
+	    },
+	    (Some(_), _) => panic!("Invalid file '{}'. Probable cause: file is not a rust file.",
+				   file.to_string_lossy()),
+	    (None, _) => panic!("Invalid file '{}'. Probable cause: is not a regular file.",
+				file.to_string_lossy()),
+	}
 	self
     }
-    pub fn add_files(&mut self, mut files: Vec<PathBuf>) -> &mut Self {
-	self.files.append(&mut files);
+    pub fn use_file(&mut self, file: PathBuf, use_stmt: String) -> &mut Self {
+	let file = match file.canonicalize() {
+	    Ok(file) => file,
+	    Err(err) => panic!("Could not canonicalize file '{}': {}", file.to_string_lossy(), err),
+	};
+	match (file.file_stem(), file.extension()) {
+	    (Some(mod_name), Some(ext)) if ext == "rs" => {
+		self.stmts.push(format!("#[path = \"{}\"]\nmod {};\nuse {}::{};", file.to_string_lossy(),
+					mod_name.to_string_lossy(), mod_name.to_string_lossy(), use_stmt));
+	    },
+	    (Some(_), _) => panic!("Invalid file '{}'. Probable cause: file is not a rust file.",
+				   file.to_string_lossy()),
+	    (None, _) => panic!("Invalid file '{}'. Probable cause: is not a regular file.",
+				file.to_string_lossy()),
+	}
+	self
+    }
+    pub fn use_crate(&mut self, use_stmt: String) -> &mut Self {
+	self.stmts.push(format!("use {};", use_stmt));
 	self
     }
     pub fn write_to_file(&mut self, path: PathBuf) {
-	let file_strings: Vec<String> = self.files.iter().map(|s| match s.canonicalize() {
-	    Ok(absolute) => absolute.to_string_lossy().into_owned(),
-	    Err(err) => panic!("Unable to canonicalize path for file '{}': {}", s.to_string_lossy(), err),
-	}).collect();
 	match File::create(&path) {
 	    Ok(mut file) => {
-		if let Err(err) = file.write_all(quote!{
-		    proc_use::proc_use! {
-			#(mod(#file_strings);)*
-		    }
-		}.to_string().as_bytes()) {
+		if let Err(err) = file.write_all(self.stmts.join("\n").as_bytes()) {
 		    panic!("Could not write to file '{}': {}", path.to_string_lossy(), err);
 		}
 	    },
-	    Err(err) => panic!("Could not open file '{}' for writing: {}", path.to_string_lossy(), err),
+	    Err(err) => panic!("Could not open file '{}' for writing: {}",
+			       path.to_string_lossy(), err),
 	}
     }
 }
