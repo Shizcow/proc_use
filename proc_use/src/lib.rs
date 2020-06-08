d45a1e110829022cfdeed07b6c95ec5d50e63999
Shizcow <pohl.devin@gmail.com>
2020-06-07 18:10:27 -0600

    Finished adding aliases

--- proc_use/src/lib.rs
+++ proc_use/src/lib.rs
@@ -16,11 +16,19 @@ impl UseBuilder {
     }
     
     pub fn mod_file(&mut self, file: PathBuf) -> &mut Self {
-	self.file(file, None);
+	self.file(file, None, None);
 	self
     }
     pub fn use_file(&mut self, file: PathBuf, use_stmt: String) -> &mut Self {
-	self.file(file, Some(use_stmt));
+	self.file(file, Some(use_stmt), None);
+	self
+    }
+    pub fn mod_file_alias(&mut self, file: PathBuf, alias: String) -> &mut Self {
+	self.file(file, None, Some(alias));
+	self
+    }
+    pub fn use_file_alias(&mut self, file: PathBuf, use_stmt: String, alias: String) -> &mut Self {
+	self.file(file, Some(use_stmt), Some(alias));
 	self
     }
     pub fn use_crate(&mut self, use_stmt: String) -> &mut Self {
@@ -31,7 +39,7 @@ impl UseBuilder {
     pub fn mod_glob(&mut self, globstring: &str) -> &mut Self {
 	for entry in glob(globstring).expect("Failed to read glob pattern") {
 	    match entry {
-		Ok(path) => self.file(path, None),
+		Ok(path) => self.file(path, None, None),
 		Err(e) => panic!("Could not resolve glob pattern: {:?}", e),
 	    }
 	}
@@ -40,7 +48,25 @@ impl UseBuilder {
     pub fn use_glob(&mut self, globstring: &str, use_stmt: String) -> &mut Self {
 	for entry in glob(globstring).expect("Failed to read glob pattern") {
 	    match entry {
-		Ok(path) => self.file(path, Some(use_stmt.clone())),
+		Ok(path) => self.file(path, Some(use_stmt.clone()), None),
+		Err(e) => panic!("Could not resolve glob pattern: {:?}", e),
+	    }
+	}
+	self
+    }
+    pub fn mod_glob_alias(&mut self, globstring: &str, alias: String) -> &mut Self {
+	for entry in glob(globstring).expect("Failed to read glob pattern") {
+	    match entry {
+		Ok(path) => self.file(path, None, Some(alias.clone())),
+		Err(e) => panic!("Could not resolve glob pattern: {:?}", e),
+	    }
+	}
+	self
+    }
+    pub fn use_glob_alias(&mut self, globstring: &str, use_stmt: String, alias: String) -> &mut Self {
+	for entry in glob(globstring).expect("Failed to read glob pattern") {
+	    match entry {
+		Ok(path) => self.file(path, Some(use_stmt.clone()), Some(alias.clone())),
 		Err(e) => panic!("Could not resolve glob pattern: {:?}", e),
 	    }
 	}
@@ -84,20 +110,27 @@ impl UseBuilder {
 	self
     }
     
-    fn file(&mut self, file: PathBuf, use_stmt: Option<String>) {
+    fn file(&mut self, file: PathBuf, use_stmt: Option<String>, alias: Option<String>) {
 	let file = match file.canonicalize() {
 	    Ok(file) => file,
 	    Err(err) => panic!("Could not canonicalize file '{}': {}",
 			       file.to_string_lossy(), err),
 	};
-	match (file.file_stem(), file.extension()) {
+	let mod_opt = {
+	    if alias.is_some() {
+		alias
+	    } else {
+		file.file_stem().map(|o| (*o.to_string_lossy()).to_string())
+	    }
+	};
+	match (mod_opt, file.extension()) {
 	    (Some(mod_name), Some(ext)) if ext == "rs" => {
 		self.mod_stmts.push(format!("#[path = \"{}\"]\nmod {};",
 					    file.to_string_lossy(),
-					    mod_name.to_string_lossy()));
+					    mod_name));
 		if let Some(use_stmt) = use_stmt {
 		    self.use_stmts.push(format!("use {}::{};",
-						mod_name.to_string_lossy(),
+						mod_name,
 						use_stmt));
 		}
 	    },
